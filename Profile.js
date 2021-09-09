ced5cd36be3563942707d38915c3d35b014c1d3a
bmajosek <88626213+bmajosek@users.noreply.github.com>
2021-09-09 08:58:42 +0200

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios'
+function Profile() {
+    return (
+        <div>
+            <p>SIEMKA</p>
+        </div>
+    )
+}
+
+export default Profile
